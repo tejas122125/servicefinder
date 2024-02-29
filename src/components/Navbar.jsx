3b570571e65e305fd4469b34d71b5ea3bfaa6e8c
Nayakyash005 <yashpankajkumarnayak@gmail.com>
2024-03-01 02:53:00 +0530

    sd

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 export default function Navbar() {
   return (
     <header className="text-gray-400 bg-gray-900 body-font">
-      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center top-0">
+      <motion.div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center top-0">
         <a className="flex title-font font-medium items-center text-white mb-4 md:mb-0">
           <svg
             xmlns="http://www.w3.org/2000/svg"
