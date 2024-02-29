4378ff7bee990f7948139e5542d6653949bd9e49
Brijendra-Singh2003 <brijendra0369@gmail.com>
2024-03-01 03:26:10 +0530

    plzz

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,9 @@
 import React from "react";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
 
 const Home = () => {
   return (
     <>
       <div className="relative w-screen h-screen">
-        {/* <Navbar/> */}
         <section class="text-gray-400 bg-gray-900 body-font">
           <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
             <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
@@ -32,7 +29,7 @@ const Home = () => {
             </div>
             <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 hover:scale-105">
               <img
-                class="object-cover object-center rounded rounded text-lg opacity-100 hover:scale-105 transition duration-700 ease-in-out rounded-xl"
+                class="object-cover object-center rounded text-lg opacity-100 hover:scale-105 transition duration-700 ease-in-out"
                 alt="hero"
                 src="https://images.unsplash.com/photo-1504328345606-18bbc8c9d7d1?q=80&w=2070&auto=htmlFormat&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                 onload="this.classList.add('opacity-100');"
@@ -422,7 +419,6 @@ const Home = () => {
             </div>
           </div>
         </section>
-        <Footer />
       </div>
     </>
   );
