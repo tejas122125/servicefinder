c4fc162f30fbe30d6bc324b1dd5e6f8674d6c4db
Brijendra-Singh2003 <brijendra0369@gmail.com>
2024-03-01 06:09:14 +0530

    added signin button

--- src/auth/Auth.jsx
+++ src/auth/Auth.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export const ServerUrl = "http://localhost:3000";
 
 export default function SigninBtn({ classname }) {
-  return (
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    fetch("http://localhost:3000/user", { credentials: "include" })
+      .then((res) => res.json())
+      .then((data) => setUser(data));
+  }, []);
+  return user ? (
+    <h2>{user.name}</h2>
+  ) : (
     <a className={classname} href={ServerUrl + "/auth/google"}>
       Signin
     </a>
